58f93f5f7865528a3b4f54b8930985809c0cb1eb
Sam Merwan <sam.mjalaoui@gmail.com>
2025-02-14 20:28:44 +0100

    Add a default image for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,12 @@ class Book < ApplicationRecord
     has_one_attached :featured_image
     has_many :comments, dependent: :destroy
     validates :title, :author, presence: true
+
+    def display_image
+        if featured_image.attached?
+          featured_image
+        else
+          "default_book.png"
+        end
+      end
 end
