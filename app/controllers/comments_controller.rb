8c34c8baa994bc4a81f02d92bf8fd52586d3d93b
Sam Merwan <sam.mjalaoui@gmail.com>
2025-02-15 13:17:13 +0100

    update comment, only authenticated user should be able to post a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,20 @@
 class CommentsController < ApplicationController
-    def create
-      @book = Book.find(params[:book_id])
-      @comment = @book.comments.build(comment_params)
-      @comment.user = Current.user
+  before_action :authenticated?
 
-      if @comment.save
-        redirect_to @book
-      else
-        redirect_to @book, alert: "Comment cannot be empty."
-      end
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = @book.comments.build(comment_params)
+    @comment.user = Current.user
+
+    if @comment.save
+      redirect_to @book
+    else
+      redirect_to @book, alert: "Comment cannot be empty."
     end
+  end
 
-    private
-        def comment_params
-            params.expect(comment: [ :content ])
-        end
+  private
+    def comment_params
+        params.expect(comment: [ :content ])
+    end
 end
