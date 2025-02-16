15d933c4aff5e0d5ec30c89fcb0c7225d948cfac
Sam Merwan <sam.mjalaoui@gmail.com>
2025-02-16 21:18:25 +0100

    allow allow unauthenticated to change theme

--- app/controllers/theme_controller.rb
+++ app/controllers/theme_controller.rb
@@ -1,4 +1,5 @@
 class ThemeController < ApplicationController
+  allow_unauthenticated_access
     def update
       cookies[:theme] = params[:theme]
       redirect_to(request.referrer || root_path)
